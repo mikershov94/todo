99b31a6ab6e9de64f6f0dddca6b0dc15d68b5f98
mikershov94 <ershovme@gmail.com>
2019-06-06 14:17:37 +0800

    Created AddItemForm

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -1,18 +1,55 @@
 import React from 'react';
 import './AddItemForm.sass';
 
-const AddItemForm = ({ addItem }) => {
-	return(
-		<div>
-			<button 
-					type="button"
-					className="btn btn-outline-primary"
-					onClick={ () => addItem('Hello, world') }
-			>
-			Add Task
-			</button>
-		</div>
-	);
+class AddItemForm extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			label: '',
+		};
+
+		this.onLabelChange = (e) => {
+			this.setState({
+				label: e.target.value,
+			});
+		};
+
+		this.onFormSubmit = (e) => {
+			e.preventDefault();
+			this.props.addItem(this.state.label);
+			this.setState({
+				label: '',
+			})
+		};
+	}
+
+	render() {
+
+		return(
+			<form className="add-item-form"
+						onSubmit={ this.onFormSubmit } >
+				<div className="row">
+					<div className="col-10">
+						<input type="text"
+									 className="form-control"
+									 onChange={this.onLabelChange}
+									 value={ this.state.label }
+									 placeholder="What need to be done" />
+					</div>
+					<div className="col">
+						<button 
+								type="button"
+								className="btn btn-outline-primary"
+								onClick={ this.onFormSubmit }
+						>
+						Add Task
+						</button>
+					</div>
+				</div>
+			</form>
+		);
+	}
 }
+	
 
 export default AddItemForm;
